1867951c88d0d3e84d1205800af44c4e341e1f11
Jared Glaser <Jared@glaserfamily.net>
2020-05-14 12:55:50 -0400

    fixed room route issues

--- src/room/roomRouter.ts
+++ src/room/roomRouter.ts
@@ -10,12 +10,12 @@ export class RoomRouter extends AppRouter{
     //sets up the routes within this module shows an example of a route that requires authorization, and one that does not
     setupRoutes(): void {      
         const roomController: RoomController=new RoomController();
-        this.router.post("/room/create", [SecurityMiddleware.RequireAuth],roomController.CreateRoom);
-        this.router.post("/room/sendchat", [SecurityMiddleware.RequireAuth],roomController.SendRoomChat);
-        this.router.get("/room/refreshchat", [SecurityMiddleware.RequireAuth],roomController.RefreshRoomChat);
-        this.router.post("/room/joinvoice", [SecurityMiddleware.RequireAuth],roomController.JoinRoomVoice);
-        this.router.get("/room/refreshvoice", [SecurityMiddleware.RequireAuth],roomController.RefreshRoomVoice);
-        this.router.post("/room/joinvideo", [SecurityMiddleware.RequireAuth],roomController.JoinRoomVideo);
-        this.router.get("/room/refreshvideo", [SecurityMiddleware.RequireAuth],roomController.RefreshRoomVideo);
+        this.router.post("/create", [SecurityMiddleware.RequireAuth],roomController.CreateRoom);
+        this.router.post("/sendchat", [SecurityMiddleware.RequireAuth],roomController.SendRoomChat);
+        this.router.get("/refreshchat", [SecurityMiddleware.RequireAuth],roomController.RefreshRoomChat);
+        this.router.post("/joinvoice", [SecurityMiddleware.RequireAuth],roomController.JoinRoomVoice);
+        this.router.get("/refreshvoice", [SecurityMiddleware.RequireAuth],roomController.RefreshRoomVoice);
+        this.router.post("/joinvideo", [SecurityMiddleware.RequireAuth],roomController.JoinRoomVideo);
+        this.router.get("/refreshvideo", [SecurityMiddleware.RequireAuth],roomController.RefreshRoomVideo);
     }    
 }
