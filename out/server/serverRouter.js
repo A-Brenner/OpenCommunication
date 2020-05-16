52e9d40bbe23a474b5024995aacb3959178509cf
Sam.wex98 <samwex@udel.edu>
2020-05-16 16:35:51 -0400

    Server has issues

--- out/server/serverRouter.js
+++ out/server/serverRouter.js
@@ -24,10 +24,10 @@ var serverRouter = /** @class */ (function (_super) {
     }
     //sets up the routes within this module shows an example of a route that requires authorization, and one that does not
     serverRouter.prototype.setupRoutes = function () {
-        this.router.post("/server/create", [securityMiddleware_1.SecurityMiddleware.RequireAuth], serverRouter.ServerController.CreateServer);
-        this.router.post("/server/join", [securityMiddleware_1.SecurityMiddleware.RequireAuth], serverRouter.ServerController.JoinServer);
-        this.router.delete("/server/leave", [securityMiddleware_1.SecurityMiddleware.RequireAuth], serverRouter.ServerController.LeaveServer);
-        this.router.get("/server/refresh", [securityMiddleware_1.SecurityMiddleware.RequireAuth], serverRouter.ServerController.RefreshServer);
+        this.router.post("/create", [securityMiddleware_1.SecurityMiddleware.RequireAuth], serverRouter.ServerController.CreateServer);
+        this.router.post("/join", [securityMiddleware_1.SecurityMiddleware.RequireAuth], serverRouter.ServerController.JoinServer);
+        this.router.delete("/leave", [securityMiddleware_1.SecurityMiddleware.RequireAuth], serverRouter.ServerController.LeaveServer);
+        this.router.get("/refresh", [securityMiddleware_1.SecurityMiddleware.RequireAuth], serverRouter.ServerController.RefreshServer);
     };
     serverRouter.ServerController = new serverController_1.ServerController();
     return serverRouter;
